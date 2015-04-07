53fd7cf019a287fa64c3ec0269d30338361a26ff
wiliajc87 <jordythepoet@gmail.com>
2015-04-07 18:18:37 -0500

    lots of editing of participants--override added, new participant controller and view added

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -2,10 +2,45 @@ class ParticipantsController < ApplicationController
   before_action :authenticate_user!
   def index
     @q = Participant.ransack(params[:q])
+    @q.sorts = 'name asc' if @q.sorts.empty?
     @participants = @q.result(distinct: true)
   end
 
   def show
     @participant = Participant.find(params[:id])
+    @contact_status
+    if @participant.contact == true
+      @contact_status = "Yes"
+    else
+      @contact_status = "No"
+    end
+  end
+
+  def new
+    @participant = Participant.new
+  end
+
+  def create
+    @participant = Participant.new(participant_params)
+    @participant.phone.gsub!(/\D/, "")
+    if @participant.save
+      redirect_to @participant
+    else
+      render :new
+    end
+  end
+
+  def members
+    @members = []
+    Participant.all.each do |part|
+      if part.member? == true
+        @members << part
+      end
+      @members
+    end
+  end
+
+  def participant_params
+    params.require(:participant).permit!
   end
 end
--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,12 +1,13 @@
+require 'date'
+require 'active_support/all'
+
 class Participant < ActiveRecord::Base
   belongs_to :show
   belongs_to :play
   has_many :performances
-  validates :first_name, :last_name, :email, :presence => true
+  validates :name, :email, :presence => true
   validates_format_of :email, :with => /@/
-  def show_count
-    Performance.all.where(:participant_id => self.id).count
-  end
+
   def roles_performed
     roles = []
     Performance.all.where(:participant_id => self.id).each do |participation|
@@ -14,7 +15,36 @@ class Participant < ActiveRecord::Base
     end
     roles = roles.uniq
   end
+
+  def show_count
+    Performance.all.where(:participant_id => self.id).count
+  end
+
   def member?
-    self.show_count >= 6
+    if self.override
+      true
+    elsif self.show_count >= 6 && self.six_month_check
+      true
+    else
+      false
+    end
+  end
+
+  def six_month_check
+    if self.show_count
+      calc = (DateTime.now.year * 12 + DateTime.now.month) - ((self.performances.last.show.year).to_i * 12 + to_calendar_number(self.performances.last.show.month))
+      if calc < 5
+        true
+      elsif calc == 5
+        "5 Month Warning"
+      else
+        false
+      end
+    end
+  end
+
+  def to_calendar_number(string)
+    months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    (months.index(string)+1)
   end
 end
