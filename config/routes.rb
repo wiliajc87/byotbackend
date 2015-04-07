e889aab4b0881759c83f742fb31abbe8fc42e0d8
wiliajc87 <jordythepoet@gmail.com>
2015-04-07 18:19:41 -0500

    added a /members route

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  resources :shows, except: [:destroy]
-  resources :plays, except: [:destroy]
-  resources :performances, except: [:destroy]
+  get "/members", to: "participants#members"
+  resources :shows do
+    resources :plays do
+      resources :performances
+    end
+  end
   resources :participants, except: [:destroy]
   resources :users
+
+  get "/plays", to: "plays#index"
 
   root 'shows#index'
 end
